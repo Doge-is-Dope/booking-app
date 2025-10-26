57f2c8b04568e35452cc876443b372e65f4dcf62
clement.l <maple3312936@outlook.com>
2025-10-26 14:24:37 +0800

    chore: add data types

--- main.go
+++ main.go
@@ -1,2 +1,21 @@
-Print("Hello, World!")
+package main
 
+import "fmt"
+
+func main() {
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available. \n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets to attend!")
+
+	var userName string
+	var userTickets int
+	// Ask user for their name
+
+	userName = "Alice"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets. \n", userName, userTickets)
+}
